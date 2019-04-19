89ddbfdbd5aad4f0f989cea325709e40f3fdca82
y.c.choi <yc2839@naver.com>
2019-04-19 17:23:13 +0900

    수정

---
+++ 1.ReactAndRedux/learn-redux/src/store/modules/counter.js
@@ -0,0 +1,14 @@
+// 액션 타입 정의
+const CHANGE_COLOR = 'counter/CHANGE_COLOR';
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+// 액션 타입 정의
+const CHANGE_COLOR = 'counter/CHANGE_COLOR';
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+// **** 액션 생섬함수 정의
+export const changeColor = color => ({ type: CHANGE_COLOR, color });
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
